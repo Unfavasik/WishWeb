448471a3c7cfcc84f37b5c6a48a7c474acc3d0e7
Dev asik <ayatulsekh3037@gmail.com>
2026-08-31 11:34:57 +0530

    Product page fix

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ if (fs.existsSync(distDir)) {
 fs.mkdirSync(distDir);
 
 const includeExts = ['.html', '.ico', '.png', '.jpg', '.webp', '.css', '.js'];
-const exactInclude = ['manifest.json'];
+const exactInclude = ['manifest.json', '_redirects'];
 const excludeFiles = ['package.json', 'package-lock.json', 'bun.lock', 'metadata.json', 'server.js', 'build.js'];
 
 const files = fs.readdirSync('.');
